51f1b621fbb902bbf8a93691882c9f5cbc60a93f
KueiChinHuang <kuei-chin.huang@mygeorgian.ca>
2020-10-25 21:25:25 -0400

    fix redirect issue

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
     async redirects() {
       return [
         {
-          source: '/',
+          source: '/sth',
           destination: '/',
           permanent: true,
         },
