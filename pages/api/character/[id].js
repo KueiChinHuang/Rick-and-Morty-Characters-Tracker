5e53a2a7693918fa83b198f11aa28863be7dd4ee
KueiChinHuang <kuei-chin.huang@mygeorgian.ca>
2020-10-26 21:37:43 -0400

    build api endpoint but can't use in getStaticProps

---
+++ pages/api/character/[id].js
@@ -0,0 +1,18 @@
+
+import axios from 'axios'
+
+export default async function charHandler(req, res) {
+    const id = req.query.id 
+
+    const url = `https://rickandmortyapi.com/api/character/${id}`
+    let resData = {}
+    await axios.get(url)
+        .then((response) => {
+            resData = response.data
+        }).catch((err) => {
+            console.log(err)
+        })
+        
+    res.status(200).json(resData)
+}
+
---
+++ pages/api/characters.js
@@ -0,0 +1,39 @@
+
+import axios from 'axios'
+
+export default async function handler(req, res) {
+    const name = req.query.name ? req.query.name : ""
+    const status = req.query.status ? req.query.status : ""
+    const type = req.query.type ? req.query.type : ""
+    const gender = req.query.gender ? req.query.gender : ""
+
+    let chars = []
+    let nextHref = `https://rickandmortyapi.com/api/character/?`
+    
+    while(nextHref !== null) {
+        let charsData = {}
+        await axios.get(nextHref, {
+                params: {
+                    name,
+                    status,
+                    type,
+                    gender
+                }
+            })
+            .then((response) => {
+                charsData = response.data
+            }).catch((err) => {
+                console.log(err)
+            })
+        
+        console.log("charsData in getAllData: ",charsData)
+    
+        if (charsData !== null) {
+        //   const charsData = await charsResp.json()
+          nextHref = charsData.info?.next || null
+          chars = [...chars, ...charsData.results]
+        }
+      }
+
+    res.status(200).json(chars)
+}
