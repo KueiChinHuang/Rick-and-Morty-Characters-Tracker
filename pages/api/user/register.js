9ab17aca80f41fc11ea0ca1236e464a0226206ca
KueiChinHuang <kuei-chin.huang@mygeorgian.ca>
2020-10-28 10:34:40 -0400

    stuck with inserting array using query

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -1,14 +1,17 @@
-import { connectToDatabase } from "../../util/mongodb";
+import { connectToDatabase } from "../../../util/mongodb";
 
 export default async (req, res) => {
   const { db } = await connectToDatabase();
 
-  const user = await db
-    .collection("users")
-    .find({})
-    .sort({ metacritic: -1 })
-    .limit(20)
-    .toArray();
 
-  res.json(user);
+  const dbResp = await db
+  .collection("users")
+  .insertOne({
+    username: req.query.username,
+    password: req.query.password,
+    favorite: req.query.favorite,
+
+  })
+
+res.json(dbResp);
 };
