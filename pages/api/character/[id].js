6e7f903437814d94738a438314acc278288f4f23
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-10-29 23:43:00 -0400

    prettier

--- pages/api/character/[id].js
+++ pages/api/character/[id].js
@@ -1,18 +1,18 @@
-
-import axios from 'axios'
+import axios from "axios";
 
 export default async function charHandler(req, res) {
-    const id = req.query.id 
+  const id = req.query.id;
 
-    const url = `https://rickandmortyapi.com/api/character/${id}`
-    let resData = {}
-    await axios.get(url)
-        .then((response) => {
-            resData = response.data
-        }).catch((err) => {
-            console.log(err)
-        })
-        
-    res.status(200).json(resData)
+  const url = `https://rickandmortyapi.com/api/character/${id}`;
+  let resData = {};
+  await axios
+    .get(url)
+    .then((response) => {
+      resData = response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.status(200).json(resData);
 }
-
--- pages/chars/[id].js
+++ pages/chars/[id].js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Layout from '../../components/layout'
+import Head from "next/head";
+import Layout from "../../components/layout";
 // import { getAllData } from '../../lib/chars'
-import { getAllData, getCharData } from '../../lib/chars'
-import utilStyles from '../../styles/utils.module.css'
+import { getAllData, getCharData } from "../../lib/chars";
+import utilStyles from "../../styles/utils.module.css";
 
 /*
 export async function getServerSideProps(context) {
@@ -19,46 +19,50 @@ export async function getServerSideProps(context) {
 */
 
 export async function getStaticPaths() {
-  const allData = await getAllData('')
+  const allData = await getAllData("");
   const paths = allData.map((c) => ({
-    params: { id: c.id.toString() }
-  }))
+    params: { id: c.id.toString() },
+  }));
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const charResp = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`)
-  const charData = await charResp.json()
+  const charResp = await fetch(
+    `https://rickandmortyapi.com/api/character/${params.id}`
+  );
+  const charData = await charResp.json();
 
   return {
     props: {
-      charData
+      charData,
     },
-    revalidate: 604800
-  }
+    revalidate: 604800,
+  };
 }
 
-export default function Post({charData}) {
+export default function Post({ charData }) {
   return (
-      <Layout>
+    <Layout>
       {/* {console.log(charData)} */}
-        <Head>
-          <title>{charData.name}</title>
-        </Head>
-        <article>
-          <img src={charData.image}></img>
-          <h1 className={utilStyles.headingXl}>{charData.name}</h1>
-          <div className={utilStyles.lightText}>Location: {charData.location.name}</div>
-          <div>Status: {charData.status}</div>
-          <div>Species: {charData.species}</div>
-          <div>Gender: {charData.gender}</div>
-          <div>Type: {charData.type}</div>
-          <div>Origin: {charData.origin.name}</div>
-        </article>
-      </Layout>
-  )
+      <Head>
+        <title>{charData.name}</title>
+      </Head>
+      <article>
+        <img src={charData.image}></img>
+        <h1 className={utilStyles.headingXl}>{charData.name}</h1>
+        <div className={utilStyles.lightText}>
+          Location: {charData.location.name}
+        </div>
+        <div>Status: {charData.status}</div>
+        <div>Species: {charData.species}</div>
+        <div>Gender: {charData.gender}</div>
+        <div>Type: {charData.type}</div>
+        <div>Origin: {charData.origin.name}</div>
+      </article>
+    </Layout>
+  );
 }
