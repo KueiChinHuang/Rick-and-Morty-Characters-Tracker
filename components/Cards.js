cf8259cb6a99d98421158f425fab028a41f9cc1f
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-10-31 19:10:32 -0400

    update home.module.css --> cards.module.css

--- components/Cards.js
+++ components/Cards.js
@@ -1,4 +1,4 @@
-import styles from "../styles/home.module.css";
+import styles from "../styles/cards.module.css";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 import UserContext from "./UserContext";
