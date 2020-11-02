e3d2d6dfe6cfbee3fa6cc954c8c79f63914f035e
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-11-02 12:32:09 -0500

    make Favorite a component

--- components/Card.js
+++ components/Card.js
@@ -2,45 +2,10 @@ import styles from "../styles/cards.module.css";
 import Link from "next/link";
 import { useContext } from "react";
 import UserContext from "./UserContext";
-import Axios from "axios";
-import useSWR, { mutate, trigger } from "swr";
-import StarIcon from "@material-ui/icons/Star";
-import StarBorderIcon from "@material-ui/icons/StarBorder";
+import Favorite from "./Favorite";
 
 export default function Card({ character }) {
   const { uid } = useContext(UserContext);
-  const { data: favIDs } = useSWR(`/api/user/${uid}`, (url) =>
-    Axios(url).then((r) => r.data.data.favorite)
-  );
-
-  const handleFavorite = async (cid, cname) => {
-    // If this character is already one of user's favorites, REMOVE it
-    if (favIDs.includes(cid)) {
-      if (
-        window.confirm(
-          `Are you sure you want to remove ${cname} from favorite?`
-        )
-      ) {
-        await Axios.put(`/api/user/${uid}`, {
-          favorite: favIDs.filter((p) => p !== cid),
-        })
-          .then((res) => console.log("DB updated:", res.data.data.favorite))
-          .catch((error) => console.log("Failed to update DB:", error));
-      }
-    }
-
-    // If this character is NOT yet a user's favorite, ADD it
-    else {
-      await Axios.put(`/api/user/${uid}`, {
-        favorite: favIDs.concat(cid),
-      })
-        .then((res) => console.log("DB updated:", res.data.data.favorite))
-        .catch((error) => console.log("Failed to update DB:", error));
-    }
-
-    trigger(`/api/user/${uid}`);
-    trigger(`/api/user/${uid}/favorite`);
-  };
 
   return (
     <div className={styles.card}>
@@ -52,24 +17,7 @@ export default function Card({ character }) {
       />
       <div className={styles.content}>
         {/* User can manage favorite only when they login */}
-        {!uid ? (
-          <></>
-        ) : (
-          <div
-            className={styles.favorite}
-            onClick={() => handleFavorite(character.id, character.name)}
-          >
-            {favIDs && favIDs.includes(character.id) ? (
-              <div title="Remove from favorite">
-                <StarIcon color="primary" fontSize="large" />
-              </div>
-            ) : (
-              <div title="Add to favorite">
-                <StarBorderIcon color="disabled" fontSize="large" />
-              </div>
-            )}
-          </div>
-        )}
+        {!uid ? null : <Favorite character={character} />}
         <div className={styles.title}>
           {/* User can click on character only when they login */}
           {!uid ? (
---
+++ components/Favorite.js
@@ -0,0 +1,60 @@
+import styles from "../styles/favorite.module.css";
+import { useContext } from "react";
+import UserContext from "./UserContext";
+import Axios from "axios";
+import useSWR, { mutate, trigger } from "swr";
+import StarIcon from "@material-ui/icons/Star";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+
+export default function Favorite({ character }) {
+  const { uid } = useContext(UserContext);
+  const { data: favIDs } = useSWR(`/api/user/${uid}`, (url) =>
+    Axios(url).then((r) => r.data.data.favorite)
+  );
+
+  const handleFavorite = async (cid, cname) => {
+    // If this character is already one of user's favorites, REMOVE it
+    if (favIDs.includes(cid)) {
+      if (
+        window.confirm(
+          `Are you sure you want to remove ${cname} from favorite?`
+        )
+      ) {
+        await Axios.put(`/api/user/${uid}`, {
+          favorite: favIDs.filter((p) => p !== cid),
+        })
+          .then((res) => console.log("DB updated:", res.data.data.favorite))
+          .catch((error) => console.log("Failed to update DB:", error));
+      }
+    }
+
+    // If this character is NOT yet a user's favorite, ADD it
+    else {
+      await Axios.put(`/api/user/${uid}`, {
+        favorite: favIDs.concat(cid),
+      })
+        .then((res) => console.log("DB updated:", res.data.data.favorite))
+        .catch((error) => console.log("Failed to update DB:", error));
+    }
+
+    trigger(`/api/user/${uid}`);
+    trigger(`/api/user/${uid}/favorite`);
+  };
+
+  return (
+    <div
+      className={styles.favorite}
+      onClick={() => handleFavorite(character.id, character.name)}
+    >
+      {favIDs && favIDs.includes(character.id) ? (
+        <div title="Remove from favorite">
+          <StarIcon color="primary" fontSize="large" />
+        </div>
+      ) : (
+        <div title="Add to favorite">
+          <StarBorderIcon color="disabled" fontSize="large" />
+        </div>
+      )}
+    </div>
+  );
+}
