845d6e26f9a4d63f87ecc5577e77de26c05c8f33
Kuei-Chin Huang <kuei-chin.huang@mygeorgian.ca>
2020-12-06 16:55:57 -0500

    UPDATE: update favorite page according to previous changes about jwt

--- pages/favorite.js
+++ pages/favorite.js
@@ -8,18 +8,27 @@ import styles from "../styles/favorite.module.css";
 import { useStateValue } from "../context/StateProvider";
 
 function MyFavorite() {
-  const [{ user }, dispatch] = useStateValue();
-  const { data: favData } = useSWR(
-    user ? `/api/user/${user.uid}/favorite` : null,
-    (url) => Axios(url).then((r) => r.data.favData)
-  );
+  const [{ token, characters }, dispatch] = useStateValue();
+  const requestHeader = { headers: { Authorization: token } };
+
+  // Set up swr to fetch data dynamically
+  const fetcherFid = (url) =>
+    Axios(url, requestHeader).then((r) => r.data.favIDs);
+
+  const { data: favIDs } = useSWR(token ? "/api/favorite" : null, fetcherFid);
+
+  const favData =
+    characters && favIDs
+      ? characters.filter((e) => favIDs.includes(e.id))
+      : null;
 
   return (
     <Layout>
       <title>My Favorite | Rick and Morty Character Tracker</title>
       <article>
         <h1>My Favorite</h1>
-        {typeof favData == "undefined" || !favData ? (
+        {console.log(favData)}
+        {typeof favData === "undefined" || !favData ? (
           <div className={styles.loadingContainer}>
             <ReactLoading
               type={"bubbles"}
